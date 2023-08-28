25ffe48299e8983da3879c2219529c979958fe37
de-oz <101826623+de-oz@users.noreply.github.com>
2023-08-28 18:10:40 +0300

    feat(main page): implement data fetching and render the list of all games

---
+++ src/hooks/useGameList.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useGameList = () => {
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const options = {
+      url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+      headers: {
+        'X-RapidAPI-Key': '452230ef4dmshb501ea1c22a48d5p13aa42jsna34240608d26',
+      },
+    };
+
+    const getGames = async (params) => {
+      try {
+        const { data: games } = await axios.request(params);
+        setGames(games);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getGames(options);
+  }, []);
+
+  return { games, loading };
+};
--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,5 +1,21 @@
+import { useGameList } from '../hooks/useGameList';
+
 const MainPage = () => {
-  return <>MainPage</>;
+  const { games, loading } = useGameList();
+
+  return (
+    <ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        games.map(({ id, title, release_date, publisher, genre, thumbnail }) => (
+          <li key={id}>
+            {title} - {genre} - {release_date}
+          </li>
+        ))
+      )}
+    </ul>
+  );
 };
 
 export default MainPage;
