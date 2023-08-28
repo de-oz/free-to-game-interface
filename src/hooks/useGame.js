5e794b305f55733e71b5450f097c7a89a5e14dba
de-oz <101826623+de-oz@users.noreply.github.com>
2023-08-28 21:01:05 +0300

    feat(game page): request and display game details

---
+++ src/hooks/useGame.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useGame = (id) => {
+  const [game, setGame] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const options = {
+      url: 'https://free-to-play-games-database.p.rapidapi.com/api/game',
+      params: { id },
+      headers: {
+        'X-RapidAPI-Key': '452230ef4dmshb501ea1c22a48d5p13aa42jsna34240608d26',
+      },
+    };
+
+    const getGame = async (params) => {
+      try {
+        const { data: game } = await axios.request(params);
+        setGame(game);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getGame(options);
+  }, [id]);
+
+  return { game, loading };
+};
--- src/routes/GamePage.jsx
+++ src/routes/GamePage.jsx
@@ -1,5 +1,57 @@
+import { useGame } from '../hooks/useGame';
+import { useParams } from 'react-router-dom';
+
 const GamePage = () => {
-  return <>GamePage</>;
+  const { gameId } = useParams();
+  const {
+    game: {
+      title,
+      release_date,
+      publisher,
+      developer,
+      genre,
+      thumbnail,
+      screenshots,
+      minimum_system_requirements: requirements,
+    },
+    loading,
+  } = useGame(gameId);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          <li>Title: {title}</li>
+          <li>Publisher: {publisher}</li>
+          <li>Developer: {developer}</li>
+          <li>Genre: {genre}</li>
+          <li>Release date: {release_date}</li>
+          <li>
+            Screenshots:
+            <ul>
+              {screenshots.map((screenshot) => (
+                <li key={screenshot.id}>{screenshot.image}</li>
+              ))}
+            </ul>
+          </li>
+          <li>Thumbnail: {thumbnail}</li>
+          <li>
+            System requirements:
+            <ul>
+              <li>Hey</li>
+              <li>OS: {requirements.os}</li>
+              <li>Processor: {requirements.processor}</li>
+              <li>Memory: {requirements.memory}</li>
+              <li>Graphics: {requirements.graphics}</li>
+              <li>Storage: {requirements.storage}</li>
+            </ul>
+          </li>
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default GamePage;
