c807338a49615ce62395cb1446ffaf5ca704dea8
de-oz <101826623+de-oz@users.noreply.github.com>
2023-08-29 18:07:59 +0300

    fix(game page): show requirements for browser games

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -8,6 +8,7 @@ const Game = () => {
   const {
     game: {
       title,
+      platform,
       release_date,
       publisher,
       developer,
@@ -35,26 +36,47 @@ const Game = () => {
           <li>Developer: {developer}</li>
           <li>Genre: {genre}</li>
           <li>Release date: {release_date}</li>
-          <li>
-            Screenshots:
-            <ul>
-              {screenshots.map((screenshot) => (
-                <li key={screenshot.id}>{screenshot.image}</li>
-              ))}
-            </ul>
-          </li>
+
+          {Boolean(screenshots.length) && (
+            <li>
+              Screenshots:
+              <ul>
+                {screenshots.map((screenshot) => (
+                  <li key={screenshot.id}>{screenshot.image}</li>
+                ))}
+              </ul>
+            </li>
+          )}
+
           <li>Thumbnail: {thumbnail}</li>
-          <li>
-            System requirements:
-            <ul>
-              <li>Hey</li>
-              <li>OS: {requirements.os}</li>
-              <li>Processor: {requirements.processor}</li>
-              <li>Memory: {requirements.memory}</li>
-              <li>Graphics: {requirements.graphics}</li>
-              <li>Storage: {requirements.storage}</li>
-            </ul>
-          </li>
+
+          {platform.includes('Windows') && requirements.os && (
+            <li>
+              Minimum System Requirements (Windows)
+              <ul>
+                <li>OS: {requirements.os || '?'}</li>
+                <li>Processor: {requirements.processor || '?'}</li>
+                <li>Memory: {requirements.memory || '?'}</li>
+                <li>Graphics: {requirements.graphics || '?'}</li>
+                <li>Storage: {requirements.storage || '?'}</li>
+              </ul>
+            </li>
+          )}
+
+          {platform.includes('Browser') && (
+            <li>
+              Minimum System Requirements (Browser)
+              <p>
+                {title} is a browser based game and should run smoothly on practically any PC with an updated
+                web-browser.
+              </p>
+              <p>
+                If you have old hardware or software, you may still be able to play {title}, but your game experience
+                may suffer. For the best gameplay experience, we recommend the latest versions of Firefox, Chrome, or
+                Internet Explorer.
+              </p>
+            </li>
+          )}
         </ul>
       )}
     </>
